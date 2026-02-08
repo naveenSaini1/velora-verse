41a362afd27f411fa8e4a4ae2d51ceb711666ce8
Shivam <shivam@Shivams-MacBook-Air.local>
2026-02-08 10:06:02 +0530

    intial commit

---
+++ velora_verse/velora_verse/doctype/items/items.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, velora-verse and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Items(Document):
+	pass
