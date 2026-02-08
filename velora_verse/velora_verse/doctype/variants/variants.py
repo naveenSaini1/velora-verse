f12bb5c4eaa35f4c370bbfe92512cde6596d414d
Naveen Saini <naveensaini94667@gmail.com>
2026-02-08 11:59:06 +0530

    new changes

--- velora_verse/velora_verse/doctype/variants/variants.py
+++ velora_verse/velora_verse/doctype/variants/variants.py
@@ -4,6 +4,25 @@
 # import frappe
 from frappe.model.document import Document
 
+import frappe
 
 class Variants(Document):
-	pass
+    def before_save(self):
+        variant_parts = []
+        base_name 	  = ""
+        
+        
+        if self.variant_name:
+         base_name =  frappe.get_doc("Items", self.variant_name).item_name
+
+        
+        
+        for items in self.variant_values:
+            if items.values: 
+                variant_parts.append(items.values)
+        
+        if variant_parts:
+            self.title = base_name +" ".join(variant_parts)
+        
+        print(self.variant_name, "Generated variant name")
+	
